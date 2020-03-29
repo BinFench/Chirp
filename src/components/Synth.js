8ce6b904b94bee0d7e2edbf2a18c5d8286e53444
BinFench <bcfinch@edu.uwaterloo.ca>
2020-03-28 20:55:16 -0400

    Add wave visualizer, switch waveform

--- src/components/Synth.js
+++ src/components/Synth.js
@@ -29,19 +29,91 @@ function onMIDIFailure() {
 }
 
 export default function Synth() {
-  const [masterGainValue, setMasterGainValue] = useState(100);
+  var waveAnalyser;
+  var waveBufferLength;
+  var waveDataArray;
+  const waveCanvas = React.useRef(null);
+  var wavectx;
+  const waveWidth = 150;
+  const waveHeight = 100;
+  const audioWidth = 512;
+  const audioHeight = 512;
+  const waveTypes = ["sine", "square", "sawtooth", "triangle"];
+  const audioCanvas = React.useRef(null);
+
+  const [masterGainValue, setMasterGainValue] = useState(1);
   const [value, setValue] = useState(0);
+  const [sampleWave, setSampleWave] = useState({});
+  const [waveType, setWaveType] = useState(0);
 
   const initializeMasterGain = () => {
     Audio.masterGainNode.connect(Audio.context.destination);
     Audio.masterGainNode.gain.setValueAtTime(1, Audio.context.currentTime);
   };
 
+  function drawWave() {
+    waveAnalyser.getByteTimeDomainData(waveDataArray);
+    wavectx.fillStyle = "rgb(200, 200, 200)";
+    wavectx.fillRect(0, 0, waveWidth, waveHeight);
+    wavectx.lineWidth = 2;
+    wavectx.strokeStyle = "rgb(0, 0, 0)";
+    wavectx.beginPath();
+    let indexPerPixel = waveBufferLength / (waveWidth * 4);
+    for (var x = 0; x < waveWidth; x++) {
+      let v = waveDataArray[Math.round(indexPerPixel * x)] / 128.0;
+      let y = (v * waveHeight) / 2.2 + 5;
+
+      if (x === 0) {
+        wavectx.moveTo(x, y);
+      } else {
+        wavectx.lineTo(x, y);
+      }
+    }
+    wavectx.lineTo(waveCanvas.width, waveCanvas.height / 2);
+    wavectx.stroke();
+  }
+
+  function drawAudio() {
+    waveAnalyser.getByteTimeDomainData(waveDataArray);
+    wavectx.fillStyle = "rgb(200, 200, 200)";
+    wavectx.fillRect(0, 0, waveWidth, waveHeight);
+    wavectx.lineWidth = 2;
+    wavectx.strokeStyle = "rgb(0, 0, 0)";
+    wavectx.beginPath();
+    let indexPerPixel = waveBufferLength / (waveWidth * 4);
+    for (var x = 0; x < waveWidth; x++) {
+      let v = waveDataArray[Math.round(indexPerPixel * x)] / 128.0;
+      let y = (v * waveHeight) / 2.2 + 5;
+
+      if (x === 0) {
+        wavectx.moveTo(x, y);
+      } else {
+        wavectx.lineTo(x, y);
+      }
+    }
+    wavectx.lineTo(waveCanvas.width, waveCanvas.height / 2);
+    wavectx.stroke();
+  }
+
   useEffect(() => {
     if (value === 0) {
       initializeMasterGain();
       test = new Instrument();
       test.addWave(Audio);
+
+      waveAnalyser = Audio.context.createAnalyser();
+      waveAnalyser.fftSize = 2048;
+      let wave = test.waves[0];
+      wave.oscillatorNode.connect(waveAnalyser);
+      waveBufferLength = waveAnalyser.frequencyBinCount;
+      waveDataArray = new Uint8Array(waveBufferLength);
+
+      wavectx = waveCanvas.current.getContext("2d");
+      wavectx.clearRect(0, 0, waveWidth, waveHeight);
+      setTimeout(() => {
+        drawWave();
+      }, 50);
+
       if (navigator.requestMIDIAccess) {
         console.log("This browser supports WebMIDI!");
       } else {
@@ -54,23 +126,70 @@ export default function Synth() {
         masterGainValue / 100,
         Audio.context.currentTime
       );
+      let wave = test.waves[0];
+      if (wave.type !== waveTypes[waveType]) {
+        wave.type = waveTypes[waveType];
+        wave.oscillatorNode.type = waveTypes[waveType];
+        wave.oscillatorNode.disconnect(waveAnalyser);
+        waveAnalyser = Audio.context.createAnalyser();
+        waveAnalyser.fftSize = 2048;
+        wave.oscillatorNode.connect(waveAnalyser);
+        waveBufferLength = waveAnalyser.frequencyBinCount;
+        waveDataArray = new Uint8Array(waveBufferLength);
+        wavectx = waveCanvas.current.getContext("2d");
+        wavectx.clearRect(0, 0, waveWidth, waveHeight);
+        setTimeout(() => {
+          drawWave();
+        }, 50);
+      }
     }
   });
 
   return (
-    <Silver
-      diameter={70}
-      min={0}
-      max={100}
-      value={masterGainValue}
-      step={1}
-      theme={{
-        donutColor: "blue"
-      }}
-      onValueChange={setMasterGainValue}
-      className="pad-volume"
-    >
-      <label id={"my-label"}>Master Volume</label>
-    </Silver>
+    <div id="synth">
+      <Silver
+        diameter={70}
+        min={0}
+        max={100}
+        value={masterGainValue}
+        step={1}
+        theme={{
+          donutColor: "blue"
+        }}
+        onValueChange={setMasterGainValue}
+        className="pad-volume"
+      >
+        <label id={"label"}>Master Volume</label>
+      </Silver>
+      <div id="waveController">
+        <canvas ref={waveCanvas} width={audioWidth} height={audioHeight} />
+        <button
+          onClick={() => {
+            if (waveType === 0) {
+              setWaveType(3);
+            } else {
+              setWaveType(waveType - 1);
+            }
+          }}
+          className="ui button"
+        >
+          <i class="angle double left icon"></i>
+        </button>
+        {waveTypes[waveType]}
+        <button
+          onClick={() => {
+            if (waveType === 3) {
+              setWaveType(0);
+            } else {
+              setWaveType(waveType + 1);
+            }
+          }}
+          className="ui button"
+        >
+          <i class="angle double right icon"></i>
+        </button>
+      </div>
+      <canvas ref={audioCanvas} width={waveWidth} height={waveHeight} />
+    </div>
   );
 }
